012831a58296f44076b10bf950eb122cb92fe5e6
Toren Darby <torendarby@gmail.com>
2024-05-23 14:25:12 -0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,238 @@
+import Image from "next/image";
+import { attributes, react as HomeContent } from "../content/home.md";
+
+
+
+export default function Home() {
+  console.log(attributes)
+  let { title, cats } = attributes
+
+
+
+  return (
+    <main>
+      <header>
+      <nav className="s">
+          <div className="mr-4" id="menuToggle">
+            <input className="a" type="checkbox" />
+              <span></span>
+              <span></span>
+              <span></span>
+              <ul id="menu">
+                <a href="#"><li>Inicio</li></a>
+                <a href="#"><li>Productos</li></a>
+                <a href="#"><li>Reseñas</li></a>
+                <a href="#"><li>Ayuda</li></a>
+                <a href="#"><li>Contacto</li></a>
+                {/* <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a> */}
+              </ul>
+          </div>
+          {/* <ul>
+            <li>
+              <a href="PLACEHOLDER LINK">
+               <span>Inicio</span>
+              </a>
+            </li>
+            <li>
+              <a href="PLACEHOLDER LINK">
+                <span>Productos</span>
+              </a>
+            </li>
+            <li>
+              <a href="PLACEHOLDER LINK">
+                <span>Reseñas</span>
+              </a>
+            </li>
+            <li>
+              <a href="PLACEHOLDER LINK">
+                <span>Ayuda</span>
+              </a>
+            </li>
+            <li>
+              <a href="PLACEHOLDER LINK">
+                <span>Contacto</span>
+              </a>
+            </li>
+          </ul> */}
+          <div className="flex w-full justify-center  mt-14">
+            <Image
+                  className="ml-2"
+                  src="/macanazos-logo-azul-borders.svg"
+                  alt="Macanazos Logo"
+                  width={256}
+                  height={99}
+                  priority
+                />
+         </div>
+        </nav>
+        
+        
+      </header>
+
+      <div className="bg-blue w-full h-80">
+        <div className="carrusel">
+          <button id="prev" className="prev">&#8701;</button>
+          <button id="next" className="next">&#8702;</button>
+          <ul>
+            <li>
+              <a href="#"> </a>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+
+
+      <div className="bg-orange w-full h-96 mt-12">
+        {/* AQUI VA UNA LISTA DE PRODUCTOS, LOS MAS VENDIDOS */}
+        <span className="text-white">Aqui va una lista de productos, los mas vendidos</span>
+        <div>
+          {/* <Image
+          className="ml-2"
+          src="/macanazos-lona.png"
+          alt="Macanazos Lona"
+          width={256}
+          height={99}
+          /> */}
+          <h3>Producto 1</h3>
+          <p>Descripcion del producto Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero amet odit culpa adipisci vitae dolores! Explicabo accusantium nobis, repellendus iusto ex, quo quidem dignissimos quae voluptatem libero saepe iste ab. </p>
+          <h4>Desde $XX.00</h4>
+        </div>
+      </div>
+      <footer>
+        <div className="bg-black w-full h-60 mt-12">
+          <div className="Contenedor de imagen">
+            <img className="h-auto w-1/3"
+            src="/imagotipo macanazos blanco.svg"
+            alt="Macanazos Logo" 
+          ></img>
+          </div>
+          <div className="Contenedor de contactos">
+          </div>
+          
+        </div>
+      </footer>
+    </main>
+
+  //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  //     <header>
+  //       <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+  //       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+  //       <Image
+  //         className="Logo"
+  //         src="/macanazos-logo-borders.svg"
+  //         alt="Macanazos Logo"
+  //         width={256}
+  //         height={99}
+  //         priority
+  //       />
+  //         <div className="navbar">
+  //         <a
+  //         href=""
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className="mb-3 text-2xl font-semibold">
+  //           Docs{" "}
+  //           <span className="flex transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className="m-0 max-w-[30ch] text-sm opacity-50">
+  //           Find in-depth information about Next.js features and API.
+  //         </p>
+  //       </a>
+  //         </div>  
+  //       </p>
+  //     </div>
+  //     </header>
+
+
+      
+
+  //     <ul>
+  //           {cats.map((cat, k) => (
+  //             <li key={k}>
+  //               <h2>{cat.name}</h2>
+  //               <p>{cat.description}</p>
+  //             </li>
+  //           ))}
+  //         </ul>
+
+      
+
+      
+
+  //     <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+  //       <a
+  //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className="mb-3 text-2xl font-semibold">
+  //           Docs{" "}
+  //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className="m-0 max-w-[30ch] text-sm opacity-50">
+  //           Find in-depth information about Next.js features and API.
+  //         </p>
+  //       </a>
+
+  //       <a
+  //         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className="mb-3 text-2xl font-semibold">
+  //           Learn{" "}
+  //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className="m-0 max-w-[30ch] text-sm opacity-50">
+  //           Learn about Next.js in an interactive course with&nbsp;quizzes!
+  //         </p>
+  //       </a>
+
+  //       <a
+  //         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className="mb-3 text-2xl font-semibold">
+  //           Templates{" "}
+  //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className="m-0 max-w-[30ch] text-sm opacity-50">
+  //           Explore starter templates for Next.js.
+  //         </p>
+  //       </a>
+
+  //       <a
+  //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className="mb-3 text-2xl font-semibold">
+  //           Deploy{" "}
+  //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+  //           Instantly deploy your Next.js site to a shareable URL with Vercel.
+  //         </p>
+  //       </a>
+  //     </div>
+  //   </main>
+);
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      'blue': '#29499e',
+      'orange': '#f68920',
+      'red': '#9a0000',
+      'black': '#010101',
+      'white': '#fffff7'
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
