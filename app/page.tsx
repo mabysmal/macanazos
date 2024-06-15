e6d893c62c3d497f483e86c34e9880a4a64d7817
Toren Darby <torendarby@gmail.com>
2024-06-14 17:34:01 -0700

    tratando de mostrar productos que el admin creo

--- app/page.tsx
+++ app/page.tsx
@@ -9,56 +9,65 @@ import HamburgerMenu from '@/components/hamburger-menu';
 export default function Home() {
   // catalog
 
-  // const productsDirectory = path.join(process.cwd(), "content/products");
+  const productsDirectory = path.join(process.cwd(), "content/productos");
 
-  // type ProductContent = {
-  //   readonly title: string;
-  //   readonly description: string;
-  //   readonly fullPath: string;
-  // };
+  type ProductContent = {
+    readonly title: string;
+    readonly description: string;
+    readonly medidas: string;
+    readonly thumbnail: string ;
+    readonly Imagen2: string;
+    readonly Imagen3: string;
+    readonly Imagen4: string;
+    readonly Imagen5: string;
+    readonly Imagen6: string;
+    readonly fullPath: string;
+  };
 
-  // let productCache: ProductContent[];
 
-  // function fetchPostContent(): ProductContent[] {
-  //   if (productCache) {
-  //     return productCache;
-  //   }
-  //   // Get file names under /posts
-  //   const fileNames = fs.readdirSync(productsDirectory);
-  //   const allPostsData = fileNames
-  //     .filter((it) => it.endsWith(".mdx"))
-  //     .map((fileName) => {
-  //       // Read markdown file as string
-  //       const fullPath = path.join(productsDirectory, fileName);
-  //       const fileContents = fs.readFileSync(fullPath, "utf8");
 
-  //       // Use gray-matter to parse the post metadata section
-  //       const matterResult = matter(fileContents, {
-  //         engines: {
-  //           yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
-  //         },
-  //       });
-  //       const matterData = matterResult.data as {
-  //         title: string;
-  //         description: string;
-  //         fullPath: string;
-  //       };
-  //       matterData.fullPath = fullPath;
+  let productCache: ProductContent[];
 
-  //       return matterData;
-  //     });
+  function fetchProductContent(): ProductContent[] {
+    if (productCache) {
+      return productCache;
+    }
+    // Get file names under /posts
+    const fileNames = fs.readdirSync(productsDirectory);
+    const allPostsData = fileNames
+      .filter((it) => it.endsWith(".mdx"))
+      .map((fileName) => {
+        // Read markdown file as string
+        const fullPath = path.join(productsDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, "utf8");
 
-  //     return allPostsData;
-  // }
+        // Use gray-matter to parse the post metadata section
+        const matterResult = matter(fileContents, {
+          engines: {
+            yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+          },
+        });
+        const matterData = matterResult.data as {
+          title: string;
+          description: string;
+          medidas: string;
+          thumbnail: string;
+          fullPath: string;
+        };
+        matterData.fullPath = fullPath;
 
-  // console.log(
-  //   fetchPostContent()
-  // )
+        return matterData;
+      });
 
+      return allPostsData;
+  }
+
+  
   // end catalog
 
   return (
     <main>
+
       <HamburgerMenu />
       <div id="ContenedorCarrusel" className="bg-blue w-full p-4">
         <Carrousel />
@@ -70,6 +79,21 @@ export default function Home() {
           <h3>Producto 1</h3>
           <p>Descripcion del producto Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero amet odit culpa adipisci vitae dolores! Explicabo accusantium nobis, repellendus iusto ex, quo quidem dignissimos quae voluptatem libero saepe iste ab.</p>
           <h4>Desde $XX.00</h4>
+          <span>Products List</span>
+          {fetchProductContent().map((product) => (
+            <div> 
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <h4>{product.medidas}</h4>
+              <div>
+                <img src={product.thumbnail}
+                 alt="Product Image" 
+                 width={100}
+                 height={100}
+                 />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <footer>
