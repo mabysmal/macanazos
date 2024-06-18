8c05f57e325168afed8a77c328427d754c7e1d78
Toren Darby <torendarby@gmail.com>
2024-06-17 20:50:53 -0700

    carrusel listo

--- components/carrousel.tsx
+++ components/carrousel.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from "react";
 import Image from 'next/image';
-////
+/////
 const Carrousel = () => {
   const images = [
     "/banner/banner_gracias.jpg",
