0da005e13b68a82078fae7ec1265ab0aba21a748
Mabel Castillo <maby.garza@gmail.com>
2024-07-24 23:55:43 -0700

    finish proceso

--- app/proceso/page.tsx
+++ app/proceso/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import HamburgerMenu from "@/components/hamburger-menu";
 import FooterMacanazos from '@/components/footerMacanazos';
 import OrderProcess from '@/components/order-process';
+import './styles.css'
 
 export default function Home() {
 
@@ -12,6 +13,7 @@ export default function Home() {
         <HamburgerMenu></HamburgerMenu>
         <div className='bg-blue'>
             <h1 className='text-6xl py-8 text-white text-center underline decoration-orange'>Proceso para hacer tu pedido.</h1>
+            <p className='text-2xl pt-0.5 text-white text-center'>Por el momento, sólo hacemos entregas locales en Nuevo León en los siguientes municipios: Monterrey, San Pedro, San Nicolás, Guadalupe y Apodaca.</p>
             
             <OrderProcess />
 
--- components/order-process.tsx
+++ components/order-process.tsx
@@ -9,20 +9,44 @@ interface Step {
 }
 
 const steps: Step[] = [
-  { icon: '💬', title: 'Cotizar', content: `
-    Checa nuestro catálogo y contáctanos por medio de los botones de WhatsApp o email en la página de nuestros productos. 
-    Te cotizaremos tu pedido, sin compromiso.
-    Necesitaremos estos datos para hacer tu cotización:
-    - ¿De cuántas personas es el grupo a caracterizar?
-    - ¿De qué escuela, empresa o grupo deportivo son?
-    - ¿Cuál es la fecha del evento?
-    - ¿Quién o quiénes estarán a cargo de recibir el trabajo?
+  { 
+    icon: '💬', 
+    title: 'Paso 1: Cotización', 
+    content: `
+      <p className='pt-4'>Checa nuestro catálogo y contáctanos por medio de los botones de WhatsApp o email en la página de nuestros productos. 
+      Te cotizaremos tu pedido, sin compromiso.</p>
+      <p className='pt-4'>Necesitaremos estos datos para hacer tu cotización:</p>
+      <ul className='pt-6'>
+        <li className='pt-4'>🔵 ¿De cuántas personas es el grupo a caracterizar?</li>
+        <li className='pt-4'>🔵 ¿De qué escuela, empresa o grupo deportivo son?</li>
+        <li className='pt-4'>🔵 ¿Cuál es la fecha del evento?</li>
+        <li className='pt-4'>🔵 ¿Quién o quiénes estarán a cargo de recibir el trabajo?</li>
+      </ul>
+    `
+  },
+  { icon: '💲', title: 'Paso 2: Primer Pago', content:  `
+    <p>Se realiza el pago del 50% del total del pedido.</p>
+    <p>Necesitaremos estos documentos para empezar la producción:</p>
+    <ul>
+      <li>🔵 Lista de nombres de las personas a retratar.</li>
+      <li>🔵 Imágenes del rostro de las personas a retratar. Deben ser imágenes en ángulo normal, con las facciones del rostro visibles.</li>
+      <li>🔵 Si el pedido es para una escuela, empresa o club, mandar el diseño del uniforme para caracterizar.</li>
+      <li>🔵 Dependiendo del pedido, necesitaremos más datos al respecto. Este es un lugar ideal para que puedas detallar completamente cualquier aspecto que desees sobre tu pedido. Queremos personalizar tu solicitud para asegurarnos de que los productos se caractericen de la mejor manera posible. </li>
+    </ul>
   ` },
-  { icon: '💲', title: 'Quote', content: 'Well provide a quote for your order.' },
-  { icon: '🔄', title: 'Process', content: 'Your order is processed in our system.' },
-  { icon: '🚚', title: 'Ship', content: 'We ship your order to you.' },
-  { icon: '📦', title: 'Deliver', content: 'Your order is delivered.' },
-  { icon: '⭐', title: 'Review', content: 'Leave a review of your experience.' },
+  { icon: '🧑🏽‍🎨', title: 'Paso 3: Producción del pedido', content:
+     '<p>Una vez recibidos los datos que nos proporcionaste y efectuado el primer pago del 50% del total del pedido, procederemos a iniciar la fabricación de tus productos.</p> <p>Durante este proceso, te mantendremos informado con actualizaciones y fotografías del avance de la producción.</p> Además, estaremos atentos para responder cualquier pregunta adicional que puedas tener, comunicándonos a través del medio que hayas seleccionado (ya sea WhatsApp o correo electrónico). Mantente al tanto de nuestras comunicaciones para recibir información puntual..</p>' },
+  { icon: '📦', title: 'Paso 4: Entregas Locales', content:   `
+    <p>Por el momento, sólo hacemos entregas locales en Nuevo León en los siguientes municipios: Monterrey, San Pedro, San Nicolás, Guadalupe y Apodaca.</p>
+    <p>Una vez finalizado el pedido, le pediremos acordar día, hora y lugar específico de entrega.</p>
+    <p>También, le pediremos el nombre completo de la persona o personas quienes recibirán el pedido. No se entregarán pedidos a terceras personas fuera del acuerdo.</p>
+    <p>Antes de poder recibir el pedido deberá pagar el 50% restante del total del pedido. No se le entregará el pedido si no ha liquidado el pago.</p>
+  ` },
+  { icon: '⭐', title: 'Paso 5: Evalúanos', content:   `
+    <p >En Macanazos, nos dedicamos apasionadamente a capturar momentos especiales a través de caricaturas personalizadas.</p>
+    <p>Valoramos sinceramente tu opinión y tu experiencia con nosotros. Una vez que recibas tu caricatura personalizada y estés encantado con el resultado, te invitamos a compartir tu experiencia en <a class="link-review" href="https://g.page/r/CYhAC8pPsNGuEBM/review"> nuestra página de Google Maps haciendo click aquí.</a> Tu reseña no solo nos ayuda a crecer y mejorar, sino que también orienta a otros en la comunidad que buscan un servicio personalizado y memorable para sus eventos especiales.</p>
+    <p>Estamos emocionados de ser parte de tus momentos más significativos y esperamos que disfrutes cada detalle de nuestras creaciones artísticas. ¡Gracias por elegir a Macanazos para dar vida a tus recuerdos de una manera única y especial!</p>
+  ` },
 ];
 
 const ChevronLeft: React.FC = () => (
@@ -62,26 +86,27 @@ const OrderProcess: React.FC = () => {
           </React.Fragment>
         ))}
       </div>
-      <div className="bg-white rounded-lg shadow-md p-6 mb-4">
-        <h2 className="text-2xl font-bold mb-2">{steps[activeStep].title}</h2>
-        <p>{steps[activeStep].content}</p>
-      </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between py-2">
         <button
           onClick={handlePrev}
           disabled={activeStep === 0}
           className="flex items-center px-4 py-2 bg-blue-700 text-white rounded disabled:opacity-50 hover:bg-blue-800"
         >
-          <ChevronLeft /> <span className="ml-2">Previous</span>
+          <ChevronLeft /> <span className="ml-2">Paso anterior</span>
         </button>
         <button
           onClick={handleNext}
           disabled={activeStep === steps.length - 1}
           className="flex items-center px-4 py-2 bg-blue-700 text-white rounded disabled:opacity-50 hover:bg-blue-800"
         >
-          <span className="mr-2">Next</span> <ChevronRight />
+          <span className="mr-2">Siguiente paso</span> <ChevronRight />
         </button>
       </div>
+      <div className="bg-orange rounded-lg shadow-md p-6 mb-4">
+        <h2 className="text-white text-4xl font-bold mb-2">{steps[activeStep].title}</h2>
+        <div className='DescripcionPaso text-white' dangerouslySetInnerHTML={{ __html: steps[activeStep].content }} />
+      </div>
+      
     </div>
   );
 };
